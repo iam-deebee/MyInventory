ea1484444ad813f87800851d54b6c0ed49de20c2
Dinesh Babu Kalangi <dkalangi@internal.salesforce.com>
2025-01-26 07:43:55 +0530

    first push

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function MyButton() {
+  return (
+    <button>I'm a button</button>
+  );
+}
+
+export default MyButton
